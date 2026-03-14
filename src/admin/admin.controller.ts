4c825280e1b2a369f08840066ac076a491cf8f83
shyam-ga04 <shyamganeshravichandran4@gmail.com>
2026-03-14 23:00:24 +0530

    feat: create and update query for company has been implemented.

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { CompanyDetailsDto } from 'src/dto/company.dto';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Get()
+  @Get('/company')
   getCompanyDetails() {
     return this.adminService.getCompany();
+  }
+
+  @Post('/company/create')
+  @ApiBody({ type: CompanyDetailsDto })
+  createCompany(company: CompanyDetailsDto) {
+    return this.adminService.createCompany(company);
+  }
+
+  @Patch('/company/update/:id')
+  @ApiBody({ type: CompanyDetailsDto })
+  updateCompany(company: CompanyDetailsDto, @Param('id') id: string) {
+    return this.adminService.updateCompany(Number(id), company);
   }
 }
