069a4f978a63ce4400ddc365e7b132fd6692b139
shyam-ga04 <shyamganeshravichandran4@gmail.com>
2026-03-28 19:55:41 +0530

    feat: enabled cross

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Raj Constructions')
     .setDescription('The API docs Marketing site content management.')
